a1863030556d8554d571b6a605b4ef2e1165292e
Giovanni Juliao <gjuliao32@gmail.com>
2023-08-28 12:08:52 -0500

    Added links controller assing links tests

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
 
-    it 'assigns links' do
+    it 'shows all available links in index' do
         link_one = Link.new(
             original_url: 'http://example.com/breakfast',
             lookup_code: '12345'
@@ -11,8 +11,8 @@ RSpec.describe LinksController, type: :controller do
         link_one.save
 
         link_two = Link.new(
-            original_url: 'http://example.com/breakfast',
-            lookup_code: '12345'
+            original_url: 'http://example.com/breakfast-two',
+            lookup_code: '12346'
         )
 
         link_two.save
@@ -20,6 +20,18 @@ RSpec.describe LinksController, type: :controller do
         get :index
 
         expect(assigns(:links)).to eq([link_one, link_two])
+    end
+
+
+    it 'can shorten link provided by user' do
+        url = 'http://example.com/breakfast'
+        request.env['HTTP_ACCEPT'] = 'text/javascript'
+
+        post :create, params: { link: { original_url: url } }
+        link = assigns(:link)
+        expect(link.original_url).to eq(url)
+        expect(link.valid?).to be(true)
+        expect(link.lookup_code.length).to eq(5)
     end
     
 end
